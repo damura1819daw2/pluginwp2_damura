95b92b1f20ab92482f19d2ef711098d4e55343a5
Daniel Muelle Ramos <damura1819daw2@gmail.com>
2019-03-09 14:03:24 +0100

    Commit 1

---
+++ peupagina.php
@@ -0,0 +1,11 @@
+<?php
+/*
+Plugin Name: Peu de pàgina per defecte per a noves entrades
+Plugin URI: https://github.com/damura1819daw2/pluginwp2_damura
+Description: Crea peus de pàgina per defecte per a noves entrade
+Version: 0.1
+Author: Daniel Muelle Ramos
+Author URI: https://github.com/damura1819daw2
+License: GNU GPLv3
+*/
+?>
